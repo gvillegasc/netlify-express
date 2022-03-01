a1ffeb7b7cd102b4d2266a8f1b4586b26ddd605d
Gerardo Villegas <villegasgerardo.j@gmail.com>
2022-03-01 14:53:18 -0500

    add public config

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import apiRoute from "./routes/commit.route";
 const app = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,8 +14,12 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/api", function (req: Request, res: Response) {
+app.get("/api", (req: Request, res: Response) => {
   res.send("Welcome to the api :)");
+});
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
 });
 
 const router = express.Router();
