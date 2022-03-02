6bab802d639c44c263502a9094aca8e2143170be
Gerardo Villegas <villegasgerardo.j@gmail.com>
2022-03-02 12:06:27 -0500

    update url of commits:

--- src/controllers/commit.controller.ts
+++ src/controllers/commit.controller.ts
@@ -5,7 +5,7 @@ import { ICommit } from "../interfaces/commit.interface";
 export const commits = async (req: Request, res: Response) => {
   try {
     const response = await fetch(
-      "https://api.github.com/repos/gvillegasc/githubapp/commits"
+      "https://api.github.com/repos/gvillegasc/github_api_app/commits"
     );
     const commits: ICommit[] = await response.json();
     return res.status(200).json(commits);
